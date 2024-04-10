91d209705636cc291167c3b08d31e185428e2787
Niklas Widmann <nik@somethingcatchy.net>
2024-04-10 16:33:58 +0200

    correct permission check

--- src/main/kotlin/com/possible_triangle/dynmapgui/extensions/_Player.kt
+++ src/main/kotlin/com/possible_triangle/dynmapgui/extensions/_Player.kt
@@ -1,10 +1,10 @@
 package com.possible_triangle.dynmapgui.extensions
 
 import net.minecraft.server.level.ServerPlayer
-import org.dynmap.common.DynmapCommandSender
+import org.dynmap.fabric_1_20.DynmapPlugin
+import org.dynmap.fabric_1_20.FabricPlayer
 
 fun ServerPlayer.hasDynmapPermission(key: String): Boolean {
     if(hasPermissions(2)) return true
-    if(this !is DynmapCommandSender) return false
-    return hasPrivilege(key)
+    return FabricPlayer(DynmapPlugin.plugin, this).hasPrivilege(key)
 }
